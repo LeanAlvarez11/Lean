04cb19d191d5a657262b0acc6b289f44656a5934
leanalvarez11 <T11641.Training56@accenture.com>
2020-01-22 17:34:09 -0300

    .

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,3 @@
-//ejercicio 2
 console.log ("Starting Javascrip...");
 var myname="leandro";
 console.log (myname);
@@ -27,7 +26,6 @@ else
 	{
 		console.log ("tiene la misma edad que ignasi")
 	}
-//ejercicio 3
 var names = ["nahu","alan","diego","ariel","lean"]
 names.sort();														//ordena alfabeticamente el vector
 for (var i=0;i<5;i++)
@@ -68,8 +66,67 @@ function mostrarmayor (array)
 		console.log (cont);
 	}
 mostrarmayor (edades);
+var ejem = [3,6,67,6,23,11,100,8,93,0,17,24,7,1,33,45,28,33,23,12,99,100];
 function mostrarindice (array,i)
 	{
 		console.log (array[i]);										//crea una funcion que muestre un array con un indice indicado
 	}
-mostrarindice (edades,3);
+mostrarindice (ejem,1);
+function repetidos (array)
+	{
+		for (var i=0;i<array.length;i++)
+			{
+				for (var j=i+1;j<array.length;j++)					//crea una funcion que muestre los numeros repetidos
+					{
+						if (array[j]==array[i])
+							{
+								console.log (array[i]);
+							}
+					}
+			}
+	}
+repetidos (ejem);
+myColor = ["Red", "Green", "White", "Black"];
+function arraytostring (array)
+	{
+		var x = array.toString()								//crea una funcion que convierta un array en un string
+		console.log (x);
+	}
+arraytostring (myColor);
+var cadena=1234;
+function invertir(int) 
+	{
+	  var string = int.toString();
+	  var x = string.length;
+	  var cadenaInvertida = "";
+
+	  while (x>=0)
+	   	{
+		    cadenaInvertida = cadenaInvertida + string.charAt(x);	//funcion que invierte una string
+		    x--;
+		}
+	    console.log (cadenaInvertida);
+	  }
+invertir (cadena);
+var  ejem2="hola";
+function ordenar (string)
+ 	{
+ 		var vec=[];
+ 		var x = string.length;
+ 		while (x>=0)
+ 			{
+ 				x--;
+ 				vec[x]=string.charAt(x).split();	//funcion que ordene una string alfabeticamente
+ 			}
+ 		vec.sort();
+ 		string=vec.toString();
+ 		let s = string.replace(/,/g, ""); //.replace () remplaza algo "(/o/g" por otra cosa ","a")"
+ 		console.log (s);
+ 	}
+orden
+ar (ejem2); 
+function mayusculas (string)
+	{
+		var vec =[];
+		vec []=string.split();
+	}
